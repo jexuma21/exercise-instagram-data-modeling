3cedf5f9e88c117c9659c18b801646a22118a998
jexuma21 <jerryexuma21@gmail.com>
2023-01-23 03:27:13 +0000

    final commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(250) , nullable=False)
+    firstname = Column(String(250) , nullable=False)
+    lastname = Column(String(250) , nullable=False)
+    email = Column(String(250) , unique=True , nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer , primary_key=True)
+    user_id = Column(Integer , ForeignKey('user.user_id'))
+
+class Choices(enum.Enum):
+    one = 1
+    two = 2
+    three = 3
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer , primary_key=True)
+    media_type = Column(Integer) 
+    url = Column(String(250)) ##string used as placeholder until I can figure out enum
+    post_id = Column(Integer)
+
+class Comment(Base): 
+    __tablename__ = 'comment'
+    id = Column(Integer , primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer , ForeignKey('user.author_id'))
+    post_id = Column(Integer , ForeignKey('post.post_id'))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer , primary_key=True)
+    user_to_id = Column(Integer)
 
     def to_dict(self):
         return {}
